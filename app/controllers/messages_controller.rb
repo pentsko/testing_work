13a6faf1968a1cdc86b3c3f03fc7902df6efe342
Taras <pentsko.t@gmail.com>
2021-10-08 08:28:00 +0300

    add route to action show, and show only datas ids

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,29 @@
 class MessagesController < ApplicationController
 
-	def index
+	def show
 		@messages = Message.all
 		render json: @messages, only: [:id]
+
 		end
+
+	def create
+		@message = Message.new(message_params)
+
+		if @message.save
+			render json: @message, status: :created, location: @message
+		else
+			render json: @message.errors, status: :unprocessable_enitity
+		end
+	end
+
+	private
+
+	def message_params
+		params.require(:message).permit(:body)
+	end
 
 
 
 end
+
+# find_by(params[:id])
