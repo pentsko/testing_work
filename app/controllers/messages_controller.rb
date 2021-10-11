5641d1c4cb656bee6030f23a5c2c5e83b60fe594
Taras <pentsko.t@gmail.com>
2021-10-11 23:19:19 +0300

    add function - app generating a link and returns it as a response

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,17 +9,27 @@ class MessagesController < ApplicationController
 	def create
 		@messages = Message.new(message_params)
 
-		# if @messages.save
-		# 	render json: @messages, status: :created
-		# else
-		# 	render json: @messages.errors, status: :unprocessable_enitity
-		# end
+
+		if @messages.save
+			render json: "http://localhost:3000/messages/#{@messages[:id].to_s}"
+			#render json: "http://localhost:3000/messages/#{JSON.(@messages[:id])}", status: :created
+		else
+			render json: @messages.errors, status: :unprocessable_enitity
+		end
 	end
 
 	private
+
+
 	def message_params
 		params.require(:messages).permit(:body)
+
+		# @id = message_params[id]
+		# @id = respond_to do |el|
+		# 	el.json { render :json => {:id => @messages.id} }
+		# end
 	end
+
 
 end
 
