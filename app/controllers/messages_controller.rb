7b4b4244e684c4011d8f2ddcae4bb0ac9238d091
Taras <pentsko.t@gmail.com>
2021-10-12 20:49:26 +0300

    Code cleanup

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,37 +1,20 @@
 class MessagesController < ApplicationController
 
 	def show
-		@messages = Message.find(params[:id])
-		# render json: count = %x{wc -l #{@messages}}.split.first.to_i
-		# render json: @messages[:id]
-
-		# puts
-		# @count = %x{wc -l < "#{@messages[:id]}"}.to_i
-		# render json: hash[:id].count
-		# if count > 1
-		# 	@messages[:read] == true || false
-			
-		render json: @messages
-		# render json: @messages[:read] == false
-		#
-		# @messages.toggle(:read)
-		# 	 render json: @messages
-		# else
-		# 	render json: "You already request this message"
-		# end
+		@message = Message.find(params[:id])
+		render json: {body: @message.body}
 		end
 
 	def create
-		@messages = Message.new(message_params)
-		if @messages.save
-			render json: "http://localhost:3000/messages/#{@messages[:id].to_s}"
+		@message = Message.new(message_params)
+		if @message.save
+			render json: {link: "http://localhost:3000/messages/#{@message.id}"}
 		else
-			render json: @messages.errors, status: :unprocessable_enitity
+			render json: @message.errors, status: :unprocessable_enitity
 		end
 	end
 
 	private
-
 
 	def message_params
 		params.require(:messages).permit(:body)
